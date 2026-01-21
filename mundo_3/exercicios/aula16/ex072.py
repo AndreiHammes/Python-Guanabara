a5edd77a2362294da712513871f3ed8f746772e4
AndreiHammes <andreihammes1067@gmail.com>
2026-01-21 18:12:06 -0300

    adidionar exercicios 72, 73, 74

--- mundo_3/exercicios/aula16/ex072.py
+++ mundo_3/exercicios/aula16/ex072.py
@@ -1,8 +1,8 @@
 # Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por extenso, de zero até vinte.
 # Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso  
 
-numeros_extenso = ("zero", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove", "dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove", "vinte")
+numeros_extenso = ("um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove", "dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove", "vinte")
 
-numero_escolha = int(input('Digite um número entre 0 e 20: '))
+numero_escolha = int(input('Digite um número entre 1 e 20: '))
 print(f"O número escolhido foi {numeros_extenso[numero_escolha]}")
 
--- mundo_3/exercicios/aula16/ex074.py
+++ mundo_3/exercicios/aula16/ex074.py
@@ -1,2 +1,9 @@
-# Crie um programa que vai gerar cinco números aleatórios e colcoar em uma tupla.
+# Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla.
 # Depois disso, mostre a listagem de números gerados e também indique o menor e o maior valor que estão na tupla
+
+import random
+
+
+aleatorio = tuple(random.randint(0,26) for i in range(5))
+print(f"Listagem --> {aleatorio}")
+print(f'O menor valor é {min(aleatorio)} e o maior é {max(aleatorio)}')
