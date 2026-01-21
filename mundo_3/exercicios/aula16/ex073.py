62e3af6943e29c1d81c4887ea898fd26d81efd25
AndreiHammes <andreihammes1067@gmail.com>
2026-01-21 00:05:57 -0300

    resolver exercicios 72 e 73 aula 16

--- mundo_3/exercicios/aula16/ex073.py
+++ mundo_3/exercicios/aula16/ex073.py
@@ -3,3 +3,14 @@
 # os últimos 4 colocados da tablea
 # uma lista com os times em ordem alfabética
 # em que posição na tabela está o time da Chapecoense
+
+colocados_brasileirao = (
+    "Palmeiras", "Grêmio", "Atlético-MG", "Flamengo", "Botafogo", 
+    "Bragantino", "Fluminense", "Athletico-PR", "Internacional", "Fortaleza", 
+    "São Paulo", "Cuiabá", "Corinthians", "Cruzeiro", "Vasco", 
+    "Bahia", "Santos", "Goiás", "Coritiba", "América-MG"
+)
+
+print(f'\nOs 5 primeiros colocados são: {colocados_brasileirao[:5]}')
+print(f'\nOs 4 últimos colocados da tabela são: {colocados_brasileirao[-4:]}')
+print(f'\nA lista com os times em ordem alfabética fica: {sorted(colocados_brasileirao)}\n')
