e7c076dcbfcc5302b53104180ace9a998de1c047
AndreiHammes <andreihammes1067@gmail.com>
2026-01-22 13:17:22 -0300

    adicionar exercicio corrigido 75

--- mundo_3/exercicios/aula16/ex075.py
+++ mundo_3/exercicios/aula16/ex075.py
@@ -2,3 +2,21 @@
 # quantas vezes apareceu o valor 9
 # em que posição foi digitado o primeiro valor 3
 # quais são os números pares
+
+cont = 0
+
+numeros = (int(input('Digite o 1º número: ')),
+           int(input('Digite o 2º número: ')),
+           int(input('Digite o 3º número: ')),
+           int(input('Digite o 4º número: ')))
+
+print(f'O número 9 apareceu {numeros.count(9)} vezes')
+     
+
+if 3 in numeros:
+    print(f'O primeiro número 3 apareceu na posição {numeros.index(3)+1}')
+
+for i in numeros:
+    if numeros.index(i) % 2 == 0:
+        cont += 1
+print(f"temos {cont} números pares")
