8f07c9ee8c12b39fb0b7137c5300ecdc65285279
AndreiHammes <andreihammes1067@gmail.com>
2026-01-26 08:52:50 -0300

    adicionar exercicio 76

--- mundo_3/exercicios/aula16/ex076.py
+++ mundo_3/exercicios/aula16/ex076.py
@@ -1,2 +1,7 @@
 # Crie um programa que tenha uma tupla única com nomes de produtos e seus respectivos preços, na sequência.
 # No final, mostre uma listagem de preços, organizando os dados de forma tabular
+
+produtos = ("violao", 6000, "bateria", 3500, "violino", 9000, "piano", 25000) 
+
+for i in range(0, len(produtos), 2):
+    print(f"{produtos[i]:.<20} R$ {produtos[i+1]:>8.2f}")
