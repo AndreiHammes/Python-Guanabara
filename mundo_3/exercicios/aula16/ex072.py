d100f92a4f357c09b0f5ce64478bd286a1e387eb
AndreiHammes <andreihammes1067@gmail.com>
2026-01-26 08:50:36 -0300

    corrigir exercicio 72 lógica

--- mundo_3/exercicios/aula16/ex072.py
+++ mundo_3/exercicios/aula16/ex072.py
@@ -4,5 +4,5 @@
 numeros_extenso = ("um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove", "dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove", "vinte")
 
 numero_escolha = int(input('Digite um número entre 1 e 20: '))
-print(f"O número escolhido foi {numeros_extenso[numero_escolha]}")
+print(f"O número escolhido foi {numeros_extenso[numero_escolha-1]}")
 
