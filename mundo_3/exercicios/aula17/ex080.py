12ab694912d5c1a0898014545624fc204f5c8646
AndreiHammes <andreihammes1067@gmail.com>
2026-05-14 17:41:34 -0300

    tentando subir ex80

--- mundo_3/exercicios/aula17/ex080.py
+++ mundo_3/exercicios/aula17/ex080.py
@@ -1,2 +1,15 @@
 # Crie um programa onde o usuário possa digitar cinco valores numéricos e cadastre-os em uma lista. Já na posição correta de inserção(sem usar o sort())
 # No final mostre a lista ordenada na tela
+
+numeros = []
+
+
+for i in range(1,6):
+    valor = int(input(f'Digite o {i} valor: '))
+    numeros.append(valor)
+    if valor >= 0:
+        print(f'Valor adicionado ao final da lista...')
+
+
+
+
